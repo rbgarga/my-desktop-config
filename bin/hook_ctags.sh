a606227b3bb50a8cdb5fcf762277f90160c12e90
Renato Botelho <garga@FreeBSD.org>
2019-02-28 15:52:14 -0300

    Detect exctags binary on FreeBSD and use it

--- bin/hook_ctags.sh
+++ bin/hook_ctags.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+if [ -f /usr/local/bin/exctags ]; then
+	ctags=exctags
+else
+	ctags=ctags
+fi
+
 set -e
 PATH="/opt/local/bin:$PATH"
 trap "rm -f .git/tags.$$" EXIT
-ctags --tag-relative -Rf.git/tags.$$ --exclude=.git --languages=PHP,JavaScript --langmap=PHP:.php.inc >/dev/null 2>&1
+$ctags --tag-relative -Rf.git/tags.$$ --exclude=.git --languages=PHP,JavaScript --langmap=PHP:.php.inc >/dev/null 2>&1
 mv .git/tags.$$ .git/tags
