37b6073ffb8b177e58d4ad87febf718fedc0282f
Renato Botelho <garga@FreeBSD.org>
2019-02-28 16:25:52 -0300

    Install git templates

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ done
 mydir=$(realpath $(dirname $0 ))
 os=$(uname -s)
 
-conf_files="gitconfig synergy.conf vimrc"
+conf_files="gitconfig git_template synergy.conf vimrc"
 
 if [ -d "${HOME}/bin" ]; then
 	echo "${HOME}/bin is a directory, skipping... "
