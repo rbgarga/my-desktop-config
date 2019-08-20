1fe218393933631cf53af526e127c31b9d1e5a08
Renato Botelho <garga@FreeBSD.org>
2019-08-20 17:06:44 -0300

    Improve i3 config

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
-echo "$@" >> /tmp/my-debug
-
-classname="$1"
-run="$2"
-workspace="$3"
-terminal="$4"
+unset classname param run terminal
+while getopts c:t:C:T flag; do
+	case ${flag} in
+		c)
+			classname=$OPTARG
+			param="class"
+			;;
+		t)
+			classname=$OPTARG
+			param="title"
+			;;
+		C)
+			run=$OPTARG
+			;;
+		T)
+			terminal=1
+			;;
+		*)
+			echo "ERROR"
+			exit 1
+	esac
+done
 
 [ -z "$classname" ] && exit 0
 [ -z "$run" ] && exit 0
 
-id=$(xdotool search --classname $classname 2>/dev/null | head -n 1)
-
-if [ $? -ne 0 -o -z "$id" ]; then
+if i3-msg "[${param}=\"${classname}\"] focus" 2>/dev/null | grep -q ERROR; then
 	if [ -n "$terminal" ]; then
 		urxvt -im $classname -name $classname -title $classname \
 		    -n $classname -e $run &
 	else
 		$run &
-	fi
-
-else
-	if [ -n "$workspace" ]; then
-		i3-msg "workspace $workspace"
-	else
-		xdotool windowactivate $id
 	fi
 fi
