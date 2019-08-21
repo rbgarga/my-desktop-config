f950eba1ceb798b9cdb3a7482463a6e706db6408
Renato Botelho <garga@FreeBSD.org>
2019-08-21 09:56:32 -0300

    Add a xdotool/xprop method to launcher.sh

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
 
+focus() {
+	local _rc=0
+
+	if [ "${param}" = "xdotool" ]; then
+		local _idx=""
+		local _id=""
+		for _idx in $(xdotool search --name ${classname} 2>&1); do
+			if xprop -id ${_idx} | grep -q _NET_WM_DESKTOP; then
+				_id=$_idx
+				break
+			fi
+		done
+		if [ -z "${_id}" ]; then
+			return 1
+		fi
+		xdotool windowactivate ${_id} >/dev/null 2>&1
+		_rc=$?
+	else
+		if i3-msg "[${param}=\"${classname}\"] focus" 2>&1 \
+		    | grep -q ERROR; then
+			_rc=1
+		fi
+	fi
+
+	return $_rc
+}
+
 unset classname param run terminal
-while getopts c:t:C:T flag; do
+while getopts c:t:C:Tx: flag; do
 	case ${flag} in
 		c)
 			classname=$OPTARG
@@ -17,6 +44,10 @@ while getopts c:t:C:T flag; do
 		T)
 			terminal=1
 			;;
+		x)
+			classname=$OPTARG
+			param="xdotool"
+			;;
 		*)
 			echo "ERROR"
 			exit 1
@@ -26,7 +57,7 @@ done
 [ -z "$classname" ] && exit 0
 [ -z "$run" ] && exit 0
 
-if i3-msg "[${param}=\"${classname}\"] focus" 2>&1 | grep -q ERROR; then
+if ! focus; then
 	if [ -n "$terminal" ]; then
 		urxvt -im $classname -name $classname -title $classname \
 		    -n $classname -e $run &
@@ -34,5 +65,5 @@ if i3-msg "[${param}=\"${classname}\"] focus" 2>&1 | grep -q ERROR; then
 		$run &
 	fi
 	sleep 1
-	i3-msg "[${param}=\"${classname}\"] focus" >/dev/null 2>&1
+	focus
 fi
