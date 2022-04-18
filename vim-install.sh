782c84d9ab04201b4323b0d9a8b07bf03088c8c0
Renato Botelho <garga@FreeBSD.org>
2022-04-18 14:29:39 -0300

    Create vimrc on correct script

--- vim-install.sh
+++ vim-install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ! which node 2>/dev/null; then
+	echo "ERROR: node binary not found" >&2
+	exit 1
+fi
+
 mydir=$(realpath $(dirname $0 ))
 vim_dir=${HOME}/.vim
 plug_vim=${vim_dir}/autoload/plug.vim
@@ -8,6 +13,7 @@ plug_dir=${vim_dir}/vim-plug
 if [ -e $plug_vim ]; then
 	echo "${plug_vim} is present, skipping... "
 else
+	ln -sf ${mydir}/vimrc ${HOME}/.vimrc
 	mkdir -p $(dirname $plug_vim)
 	rm -rf $plug_dir
 	if ! git clone https://github.com/junegunn/vim-plug.git $plug_dir; then
