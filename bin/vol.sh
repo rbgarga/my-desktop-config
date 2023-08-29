8ca7969712610612814d1b50319b45280bdb37ec
Renato Botelho <garga@FreeBSD.org>
2023-08-29 17:45:37 -0300

    vol.sh: Make it work with modern mixer

--- bin/vol.sh
+++ bin/vol.sh
@@ -3,56 +3,28 @@
 echo "$@" >> /tmp/debug
 
 action=$1
-cur_vol=$(mixer -S vol | cut -d: -f2)
-
-save_vol() {
-	if [ "$cur_vol" = "0" ]; then
-		return
-	fi
-
-	echo "$cur_vol" > /tmp/.saved_vol
-}
-
-restore_vol() {
-	local _vol=50
-	if [ -f /tmp/.saved_vol ]; then
-		_vol=$(cat /tmp/.saved_vol)
-	fi
-
-	echo "$_vol"
-}
 
 case "$action" in
 	up)
-		vol="+1"
+		cmd="vol.volume=+0.1"
 		;;
 	down)
-		vol="-1"
+		cmd="vol.volume=-0.1"
 		;;
-	[0-9]*)
-		vol="$action"
+	[0-9.]*)
+		cmd="vol.volume=$action"
 		;;
 	mute)
-		save_vol
-		vol="0"
+		cmd="vol.mute=1"
 		;;
 	unmute)
-		vol=$(restore_vol)
+		cmd="vol.mute=0"
 		;;
 	toggle)
-		if [ "$cur_vol" = "0" ]; then
-			exec $0 unmute
-		else
-			exec $0 mute
-		fi
-		exit 0
-		;;
-	show)
-		echo "$cur_vol"
-		exit 0
+		cmd="vol.mute=^"
 		;;
 	*)
 		exit 1
 esac
 
-mixer vol "$vol" >/dev/null 2>&1
+mixer $cmd >/dev/null 2>&1
