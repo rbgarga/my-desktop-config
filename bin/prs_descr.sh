6df73528f07550d8988e9770b2e048753b116bcc
Renato Botelho <garga@FreeBSD.org>
2024-04-17 15:55:03 -0300

    Remove obsolete scripts

--- bin/prs_descr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for pr in "$@"; do
-	descr=$(curl https://github.com/pfsense/pfsense/pull/${pr} 2>/dev/null | sed '/<title>/!d; s,^.*<title>,,; s,...Pull Request #.*$,,')
-	echo "  - #${pr}: ${descr}"
-done
-
